a7c2ad3982c7095fe825b484e1de6c43cd9647c5
Jonathon Nordquist <cugamer@gmail.com>
2016-03-27 12:05:40 -0500

    Add UML diagram.

--- dp_iterator_zoo_animals/src/dp_iterator_zoo_animals/Iterator.java
+++ dp_iterator_zoo_animals/src/dp_iterator_zoo_animals/Iterator.java
@@ -1,5 +1,8 @@
 package dp_iterator_zoo_animals;
 
+//  Home built iterator interface, no longer used after switching
+// to java.util.Interface.  Kept as artifact.
+
 public interface Iterator {
 	public boolean hasNext();
 	public Animal next();
